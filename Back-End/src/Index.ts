a0014e576d776b9e9e703d6b707e1d365c887fca
Carlos Farias <carlosroberto.farias@hotmail.com>
2022-05-23 19:45:34 -0300

    Adicionado TS

--- Back-End/src/Index.ts
+++ Back-End/src/Index.ts
@@ -1,1 +1,11 @@
-console.log('teste')
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express()
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
+    res.status(200).send({foo: 'Agora eu mudei'})
+});
+
+app.listen(9090,() =>{
+    console.log('Executando na porta: 9090')
+});
